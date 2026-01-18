a8e69d2074c7ae9f8e8a75c4dbc6d32765a8837e
Marcio Cruz de Almeida <marciocadev@gmail.com>
2026-01-18 11:39:22 -0300

    Initial commit

---
+++ test/aws-apigateway-lambda-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as AwsApigatewayLambdaCdk from '../lib/aws-apigateway-lambda-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/aws-apigateway-lambda-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new AwsApigatewayLambdaCdk.AwsApigatewayLambdaCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
